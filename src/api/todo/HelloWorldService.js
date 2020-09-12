87ec0ac909c16eb8f7d43cb218ceb1a13b0fea86
“rahul.solanki” <rahul.solanki@vlinkinfo.com>
2020-09-12 21:46:04 +0530

    changes

--- src/api/todo/HelloWorldService.js
+++ src/api/todo/HelloWorldService.js
@@ -4,19 +4,19 @@ class HelloWorldService{
 
     executeHelloWorldService(){
 
-       return axios.get('http://ws.informaticsolutions.in/hello-world')
+       return axios.get('https://ws.informaticsolutions.in/hello-world')
         //console.log('execute servicw')
     }
 
     executeHelloWorldBeanService(){
 
-        return axios.get('http://ws.informaticsolutions.in/hello-world-bean')
+        return axios.get('https://ws.informaticsolutions.in/hello-world-bean')
          //console.log('execute servicw')
      }
 
      executeHelloWorldPathVariableService(name){
 
-        return axios.get(`http://ws.informaticsolutions.in/hello-world/path-variable/${name}`)
+        return axios.get(`https://ws.informaticsolutions.in/hello-world/path-variable/${name}`)
          //console.log('execute servicw')
      }
 
--- src/api/todo/ToDoDataService.js
+++ src/api/todo/ToDoDataService.js
@@ -5,13 +5,13 @@ class ToDoDataService{
 retrieveAllToDos(name){
 
 
-    return axios.get(`http://ws.informaticsolutions.in/users/${name}/todos`)
+    return axios.get(`https://ws.informaticsolutions.in/users/${name}/todos`)
 }
 
 deleteToDO(name,id){
 
 
-    return axios.delete(`http://ws.informaticsolutions.in/users/${name}/todos/${id}`)
+    return axios.delete(`https://ws.informaticsolutions.in/users/${name}/todos/${id}`)
 }
 
 
